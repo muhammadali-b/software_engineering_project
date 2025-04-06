8e25f4d53077a8c2ab745bbe5c9ff37dd62a2a14
Jeffersonszn43 <xenoblade61097@gmail.com>
2025-04-06 00:24:07 -0400

    Now working on the homepages

--- Android/app/src/main/java/com/example/carbotrackphoneapplication/LoginActivity.java
+++ Android/app/src/main/java/com/example/carbotrackphoneapplication/LoginActivity.java
@@ -3,17 +3,33 @@ package com.example.carbotrackphoneapplication;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.text.InputType;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 import android.text.Html;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // Here is where we are holding the role that came from the signup page.
+    private String userRole = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Here is where we are getting the role of the user if they came from the signup page.
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("role"))
+        {
+            userRole = intent.getStringExtra("role");
+            Toast.makeText(this, "Registered as: " + userRole, Toast.LENGTH_SHORT).show();
+        }
 
         // Here is where we are adding a click event for when users will have to create an account
         TextView txtSignup = findViewById(R.id.txt_signup);
@@ -22,11 +38,22 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        //Here is where users will get directed to the home page of the app when users login
+        // Here is logic that will sign in the user to the appropriate home page on the app.
         Button btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, MainHomeActivity.class);
-            startActivity(intent);
+            // Add logic to validate the password of the user.
+
+            if(userRole.equalsIgnoreCase("Employer"))
+            {
+                Intent employer_intent = new Intent(LoginActivity.this, );
+                startActivity(employer_intent);
+            }
+            else
+            {
+                Intent employee_intent = new Intent(LoginActivity.this, );
+                startActivity(employee_intent);
+            }
+            finish();
         });
 
         // Here is where users will get directed to the forgot password page if users forget their password to login.
@@ -34,6 +61,24 @@ public class LoginActivity extends AppCompatActivity {
         txtForgotPassword.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
             startActivity(intent);
+        });
+
+        // Here is where we are going to add the functionality of having users see the password that they are typing using the eye icon.
+        EditText etPassword = findViewById(R.id.etPassword);
+        ImageView imgTogglePassword = findViewById(R.id.imgTogglePassword);
+
+        imgTogglePassword.setOnClickListener(view -> {
+            if (etPassword.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD))
+            {
+                etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                imgTogglePassword.setImageResource(R.drawable.ic_eye_open);
+            }
+            else
+            {
+                etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                imgTogglePassword.setImageResource(R.drawable.ic_eye_closed);
+            }
+            etPassword.setSelection(etPassword.getText().length());
         });
 
 
--- Android/app/src/main/java/com/example/carbotrackphoneapplication/SignupActivity.java
+++ Android/app/src/main/java/com/example/carbotrackphoneapplication/SignupActivity.java
@@ -1,14 +1,29 @@
 package com.example.carbotrackphoneapplication;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.text.InputType;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.text.Html;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsCompat.Insets;
 
 public class SignupActivity extends AppCompatActivity {
+
+    String[] roles = {"Employer", "Employee"};
+    String selectedRole="";
+
+    AutoCompleteTextView autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,55 @@ public class SignupActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Here is where we are creating the dropdown menu for users to choose whether they are an Employee or an Employer.
+        autoCompleteTextView = findViewById(R.id.auto_complete_txt);
+        Button signUpButton = findViewById(R.id.btnSignUp);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, roles);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnItemClickListener((adapterView, view, position, id) -> {
+            selectedRole = adapterView.getItemAtPosition(position).toString();
+            Toast.makeText(SignupActivity.this, "Selected Role: " + selectedRole, Toast.LENGTH_SHORT).show();
+        });
+
+
+        signUpButton.setOnClickListener(view -> {
+            //Here we are checking to see if the user has selected a user role.
+            if (selectedRole.isEmpty())
+            {
+                Toast.makeText(SignupActivity.this, "Please select a role.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Here is where the logic for if an Employer registers on the platform, then a Carbon Credit Bank will have to approve that registration.
+            // If an Employee registers on the platform then the Employer will have to go and approve the registration of the Employee.
+            if(selectedRole.equals("Employer"))
+            {
+                Toast.makeText(SignupActivity.this, "Signed up as an Employer. Now awaiting Carbon Credit Bank approval.", Toast.LENGTH_LONG).show();
+                //Once approved then do the lines below
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else if (selectedRole.equals("Employee"))
+            {
+                Toast.makeText(SignupActivity.this, "Signed up as an Employee. Now awaiting Employer approval.", Toast.LENGTH_LONG).show();
+                //Once approved then do the lines below
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
+        // Here is where users will get directed straight to the login page if they have an account already on the app.
+        TextView txt_Login = findViewById(R.id.txtLogin);
+        txt_Login.setOnClickListener(view -> {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
