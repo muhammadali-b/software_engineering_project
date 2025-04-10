9b974d233f73b36d0e2639fa843f3ffa9136e449
Chakira Morris <Chakira.morris@gmail.com>
2025-04-10 06:31:36 -0400

    Added more webpages, updated css files, and add login functionality

--- Web/scripts.js
+++ Web/scripts.js
@@ -9,7 +9,7 @@ document.getElementById("mybtn").addEventListener("click", function(e) {
     if (username === "admin" && password === "password123") {
         //Open dashboard homepage when credentials are validated and login button is clicked
         document.getElementById("mybtn").addEventListener("click", function(){
-          window.location.href = "http://127.0.0.1:5500/Web/dashboard.html";
+          window.location.href = "http://127.0.0.1:5500/Web/user_manage.html";
         });
     } else {
         // Show error messages
@@ -18,26 +18,3 @@ document.getElementById("mybtn").addEventListener("click", function(e) {
     }
 });
 
-document.getElementById("myinput").addEventListener("click", function(e) {
-  alert("Button worked");
-  // Declare variables
-  //var input, filter, table, tr, td, i, txtValue;
-  //input = document.getElementById("myInput");
-  //filter = input.value.toUpperCase();
-  //table = document.getElementById("myTable");
-  //tr = table.getElementsByTagName("tr");
-//
-  //// Loop through all table rows, and hide those who don't match the search query
-  //for (i = 0; i < tr.length; i++) {
-  //  td = tr[i].getElementsByTagName("td")[0];
-  //  if (td.innerHTML.toUpperCase().indexOf(filter)) {
-  //    txtValue = td.textContent || td.innerText;
-  //    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-  //      tr[i].style.display = "";
-  //    } 
-  //    else {
-  //      tr[i].style.display = "none";
-  //    }
-  //  }
-  //}
-});
