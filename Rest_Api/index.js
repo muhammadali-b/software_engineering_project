f24964e6e3788b06bd0b6f4fc80064387a504059
Z <z@Macmini.attlocal.net>
2025-04-13 14:25:14 -0400

    hotfix

--- Rest_Api/index.js
+++ Rest_Api/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const employeeRoutes = require('./routes/employee');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api', authRoutes);
 app.use('/api', employeeRoutes);
