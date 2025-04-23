b1a4d5338b068f9b39f467ebca7c49505596db88
Chakira Morris <Chakira.morris@gmail.com>
2025-04-22 22:45:32 -0400

    Updated web files

--- Web/scripts.js
+++ Web/scripts.js
@@ -1,31 +1,50 @@
-const web_api = "https://softwareengineeringproject-production.up.railway.app/api/register";
+//const loginForm = document.querySelector('.form');
+//
+//loginForm.addEventListener('submit', event => {
+//  event.preventDefault();
+//
+//  const formData = new FormData(loginForm);
+//  const data = Object.fromEntries(formData);
+//
+//  fetch('https://softwareengineeringproject-production.up.railway.app/api/login', {
+//    method: 'POST',
+//    headers : {
+//      'Content-Type':'application/json'
+//    },
+//    body: JSON.stringify(data)
+//  });
+//});
 
-fetch(web_api)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
 
+
+//const web_api = "https://softwareengineeringproject-production.up.railway.app/api/register";
+//
+//fetch(web_api)
+//  .then(response => {
+//    if (!response.ok) {
+//      throw new Error('Network response was not ok');
+//    }
+//    return response.json();
+//  })
+//  .then(data => {
+//    console.log(data);
+//  })
+//  .catch(error => {
+//    console.error('Error:', error);
+//  });
+//
 document.getElementById("mybtn").addEventListener("click", function(e) {
     e.preventDefault();
 
     // Get the input values
-    const username = document.getElementById("username").value;
+    const username = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     // Validate username and password. Remember to replace with an authentication process)
-    if (username === "admin" && password === "password123") {
+    if (username === "Cmorris@gmail.com" && password === "Password97") {
         //Open dashboard homepage when credentials are validated and login button is clicked
         document.getElementById("mybtn").addEventListener("click", function(){
-          window.location.href = "http://127.0.0.1:5500/Web/user_manage.html";
+          window.location.href = "http://127.0.0.1:5500/Web/dashboard.html";
         });
     } else {
         // Show error messages
@@ -33,5 +52,3 @@ document.getElementById("mybtn").addEventListener("click", function(e) {
         document.getElementById("error-message").style.display = "block";
     }
 });
-
-fetch('https://regres.in/api/users');
